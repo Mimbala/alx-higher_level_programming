c40cc05626cef12d58c2c7fb27908144321433e8
MIMBALA <mimbalao@gmail.com>
2022-08-29 18:34:51 -0700

    not all

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    """This function takes a list data type as an arguement snd prints the content of the one on a single line"""
+    for a in my_list:
+        print("{:d}".format(my_list[a]))
+
---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,7 @@
+#!/use/bin/pyyhon3
+
+def replace_in_list(my_list, idx, element):
+    """This functon rwplaces an element of a list at a specific position."""
+    if idx >= 0 and idx < len(my_list):
+        my_list[idx] = element
+    return (my_list)
